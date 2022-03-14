21380796d8859d772a6ccb425beee4983fb9b99d
Zhichen Wen <zhichenw@hotmail.com>
2022-03-14 15:35:25 +0800

    edited home header and fixed sass dependencies

--- src/components/Tasks/Header.vue
+++ src/components/Tasks/Header.vue
@@ -3,11 +3,36 @@
 
 <template>
     <div id="title">我的任务</div>
+    <div id="setting">
+        <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z"></path><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"></path></svg>
+    </div>
 </template>
 
-<style scoped>
-    #title {
-        font-size: 28px;
-        font-weight: bold;
+<style scoped lang="scss">
+@mixin desktopPosition {
+    top: 56px;
+    right: 40px;
+}
+@mixin mobilePosition {
+    top: 36px;
+    right: 20px;
+}
+
+#title {
+    font-size: 28px;
+    font-weight: bold;
+}
+#setting {
+    position: absolute;
+    @include desktopPosition;
+    width: 28px;
+    height: 28px;
+
+    cursor: pointer;
+}
+@media only screen and (max-width: 400px) {
+    #setting {
+        @include mobilePosition;
     }
+}
 </style>
